0161b6d5bc08d307f5131953bb9889bb629ccddd
Keith Kurak <keith.kurak@gmail.com>
2023-03-27 19:35:00 -0400

    fix some tracking bugs

--- app/stores/analytics.js
+++ app/stores/analytics.js
@@ -1,24 +1,43 @@
 import { init, identify, Identify, track } from '@amplitude/analytics-react-native';
-import Updates from 'expo-updates';
+import * as Updates from 'expo-updates';
 import Constants from 'expo-constants';
 
 const common = require('../common');
 
 const amplitudeAvailable = Constants.expoConfig.extra && Constants.expoConfig.extra.amplitudeApiKey;
 
-if (Constants.expoConfig.extra && Constants.expoConfig.extra.amplitudeApiKey) {
-  init(Constants.expoConfig.extra.aplitudeApiKey);
+if (amplitudeAvailable) {
+  init(Constants.expoConfig.extra.amplitudeApiKey);
 
   const identifyObj = new Identify();
   identifyObj.set('updateId', Updates.updateId);
   identifyObj.set('channel', Updates.channel);
   identifyObj.set('firstWord', common[0].data[0].translation);
   identify(identifyObj);
+
+  track('AppLaunched', {
+    updateId: Updates.updateId,
+    channel: Updates.channel,
+    firstWord: common[0].data[0].translation,
+  });
+
+  /*
+    // For update acceptance, add this event
+    track('ReceivedTargetedUpdate', {
+      updateId: Updates.updateId,
+      channel: Updates.channel,
+      firstWord: common[0].data[0].translation,
+    });
+  */
 }
 
-function trackAnalyticsEvent(event) {
+function trackAnalyticsEvent(event, properties) {
   if (amplitudeAvailable) {
-    track(event);
+    if (properties) {
+      track(event, properties);
+    } else {
+      track(event);
+    }
   }
 }
 
