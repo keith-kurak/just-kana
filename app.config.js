a61f247da74517d0e612a1eb4b8717a0ce985164
Keith Kurak <keith.kurak@gmail.com>
2023-08-15 13:33:14 -0400

    updated expo version

--- app.config.js
+++ app.config.js
@@ -4,9 +4,7 @@ module.exports = ({ config }) => {
     extra: {
       ...config.extra,
       amplitudeApiKey: process.env.EXPO_PUBLIC_AMPLITUDE_API_KEY,
-      codeName: process.env.EXPO_PUBLIC_CODE_NAME,
-      testValA: process.env.EXPO_PUBLIC_TEST_VAL_A,
-      testValB: process.env.EXPO_PUBLIC_TEST_VAL_B,
+      codeName: process.env.EXPO_PUBLIC_CODE_NAME
     }
   };
 };
