0d56f8e7c5bfd3029afe1e3bc3c746c874c86ce2
Keith Kurak <keith.kurak@gmail.com>
2023-08-15 16:57:58 -0400

    test event

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ Sentry.init({
   debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 });
 
+Sentry.Native.captureMessage('test event')
+
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
