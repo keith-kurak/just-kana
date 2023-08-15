1df3b7144cd1fec2bd03507dcd5694973413db97
Keith Kurak <keith.kurak@gmail.com>
2023-08-15 13:59:48 -0400

    add sentry init

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import Screens from './app/screens';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://1ecb149b0d21ed992c4b9851438fc797@o1310900.ingest.sentry.io/4505705506013184',
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
 
 export default function App() {
   return (
