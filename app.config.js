ec9105745a551f40d2af4494e54bd910bb10ab62
Keith Kurak <keith.kurak@gmail.com>
2023-09-01 09:25:24 -0400

    separate dev bundle Id

--- app.config.js
+++ app.config.js
@@ -1,6 +1,15 @@
 module.exports = ({ config }) => {
   return {
     ...config,
+    name: process.env.DEV_VERSION ? `${config.name} - DEV` : config.name,
+    ios: {
+      ...config.ios,
+      bundleIdentifier: process.env.DEV_VERSION ? `${config.ios.bundleIdentifier}-dev` : config.ios.bundleIdentifier,
+    },
+    android: {
+      ...config.android,
+      package: process.env.DEV_VERSION ? `${config.android.package}dev` : config.android.package,
+    },
     extra: {
       ...config.extra,
       amplitudeApiKey: process.env.EXPO_PUBLIC_AMPLITUDE_API_KEY,
