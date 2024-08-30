ba45bf3bf07d6475a4e3ffe576cd28f45145bd01
Keith Kurak <keith.kurak@gmail.com>
2024-08-30 18:29:32 -0400

    turn off sentry it breaks stuff crazy rn

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import Screens from './app/screens';
 import * as Sentry from 'sentry-expo';
 
-Sentry.init({
+/*Sentry.init({
   dsn: 'https://1ecb149b0d21ed992c4b9851438fc797@o1310900.ingest.sentry.io/4505705506013184',
   enableInExpoDevelopment: true,
   debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
-});
+});*/
 
-Sentry.Native.captureMessage('test event')
+// super-cryptic sentry error
+
+//Sentry.Native.captureMessage('test event')
 
 export default function App() {
   return (
