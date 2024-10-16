a542b90305a811e84b3a092d25bcbde948610ed9
Keith Kurak <keith.kurak@gmail.com>
2024-10-16 13:57:08 -0400

    app config changess

--- app.config.js
+++ app.config.js
@@ -1,4 +1,10 @@
 module.exports = ({ config }) => {
+  const myPlugins = config.plugins;
+  myPlugins.push([
+    "expo-dev-client", {
+      addGeneratedScheme: process.env.DEV_VERSION
+    }
+  ])
   return {
     ...config,
     name: process.env.DEV_VERSION ? `${config.name} - DEV` : config.name,
@@ -10,6 +16,7 @@ module.exports = ({ config }) => {
       ...config.android,
       package: process.env.DEV_VERSION ? `${config.android.package}dev` : config.android.package,
     },
+    plugins: myPlugins,
     extra: {
       ...config.extra,
       amplitudeApiKey: process.env.EXPO_PUBLIC_AMPLITUDE_API_KEY,
