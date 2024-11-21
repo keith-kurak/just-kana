051da9bc14ee86072b1bb855ff0d48155859d695
Keith Kurak <keith.kurak@gmail.com>
2024-11-21 18:34:20 -0500

    unique package

--- App.js
+++ App.js
@@ -47,7 +47,7 @@ export default function App() {
       // loading its initial state and rendering its first pixels. So instead,
       // we hide the splash screen once we know the root view has already
       // performed layout.
-      SplashScreen.hide();
+      SplashScreen.hideAsync();
     }
   }, [appIsReady]);
 
