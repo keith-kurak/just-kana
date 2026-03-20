8d41267328f952497edf01f2fa4113a52c2bad58
Keith Kurak <keith.kurak@gmail.com>
2026-03-20 14:22:19 -0400

    sentry added

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,19 +4,28 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { View } from 'react-native';
 import { ThemeProvider } from '@/config/styles';
 import '@/utils/background-updater';
-import { isRunningInExpoGo } from 'expo';
 import { Slot } from 'expo-router';
 import AppMetrics from 'expo-eas-observe';
+import * as Sentry from '@sentry/react-native';
 
-/*Sentry.init({
-  dsn: 'https://1ecb149b0d21ed992c4b9851438fc797@o1310900.ingest.sentry.io/4505705506013184',
-  enableInExpoDevelopment: true,
-  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
-});*/
+Sentry.init({
+  dsn: 'https://1ecb149b0d21ed992c4b9851438fc797@o1310900.ingest.us.sentry.io/4505705506013184',
 
-// super-cryptic sentry error
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
 
-//Sentry.Native.captureMessage('test event')
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 SplashScreen.setOptions({
   duration: 500,
